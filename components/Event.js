b363db347a756c156cf26e86c4d20e9148908b17
= <siheonl@andrew.cmu.edu>
2019-07-18 00:13:51 +0900

    Ainitial code for an event component, has not added any CSS

---
+++ components/Event.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import LocationInput from './location-input';
+import TimeInput from './time-input';
+import ColorInput from './color-input';
+
+class Event extends React.Component {
+  render () {
+    return (
+      <div>
+          {/* Not sure what is going inside the banner so I left it blank*/}
+        <div className="banner">
+                  
+        </div>
+
+        <div className="two-column form">
+          <input name="event-name" value="Enter Event Name"> </input>
+          <button className="event-button">Save</button>
+        </div>
+        <div className="two-column">
+          <TimeInput />
+          <LocationInput />
+        </div>
+        <div className="one-column align-right">
+          <ColorInput />
+        </div>
+        <div className="one-column">
+          <input type="text" name="description-box" value="Add description..." required/>
+        </div>
+        <div className="one-column">
+          <a className="add-link-text" href="">Add Links</a>
+          <label className="switch">
+            Make Event RSVP
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+          <select name="rsvp-list">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+          </select>
+        </div>
+        <div className="one-column align-right">
+          <button>Delete</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Event;
