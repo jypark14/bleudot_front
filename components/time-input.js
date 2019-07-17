426162b2d8d78512d0f5036a714d01de49b8316a
= <siheonl@andrew.cmu.edu>
2019-07-17 23:46:04 +0900

    created a new TimeInput component for an event component"

---
+++ components/time-input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Clock from './icons/clock';
+
+class TimeInput extends React.component {
+	render() {
+		return (
+			<div className="time-input">
+				<Clock /><span className="icon-label">Time</span>
+				<label for="start-time-input">Start Time: </label>
+				<input type="time" name="start-time-input" required>
+				</input>
+				<label for="end-time-input">End Time: </label>
+				<input type="time" name="end-time-input" required>
+				</input>
+			</div>
+		)
+	}
+}
+
+export default TimeInput;
