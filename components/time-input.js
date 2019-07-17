a78b428202f7af3faeffb333f055883217eed1d5
= <siheonl@andrew.cmu.edu>
2019-07-18 00:06:12 +0900

    fixed the label tag following its syntax

--- components/time-input.js
+++ components/time-input.js
@@ -6,12 +6,16 @@ class TimeInput extends React.component {
 		return (
 			<div className="time-input">
 				<Clock /><span className="icon-label">Time</span>
-				<label for="start-time-input">Start Time: </label>
-				<input type="time" name="start-time-input" required>
-				</input>
-				<label for="end-time-input">End Time: </label>
-				<input type="time" name="end-time-input" required>
-				</input>
+				<label for="start-time-input">Start Time: 
+					<input type="time" name="start-time-input" required>
+					</input>
+				</label>
+
+				<label for="end-time-input">End Time: 
+					<input type="time" name="end-time-input" required>
+					</input>
+				</label>
+				
 			</div>
 		)
 	}
