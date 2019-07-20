b5ca9b6680c06f3a7b65e3fc555dac5e750e55d1
= <siheonl@andrew.cmu.edu>
2019-07-20 22:49:50 +0900

    Added a new css for a label, will consider to make all the labels as one component

--- components/location-input.js
+++ components/location-input.js
@@ -12,6 +12,20 @@ class LocationInput extends React.Component {
 						<option value="sample1" selected>Sample 1</option>
 						<option value="sample1">Sample 2</option>
 				</select>
+
+				<style jsx> {`
+					label {
+						margin-left: 4px;
+						font-family: Roboto;
+						font-style: normal;
+						font-weight: normal;
+						font-size: 14px;
+						line-height: 16px;
+						color: #5F5F5F;
+					}
+				`}
+
+				</style>
 			</div>
 		)
 	}
