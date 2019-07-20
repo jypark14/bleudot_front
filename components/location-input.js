e81edcc8e52a4b065494172cf462299cf035065a
= <siheonl@andrew.cmu.edu>
2019-07-20 23:25:27 +0900

    added CSS on location input, changing div structure for better styling

--- components/location-input.js
+++ components/location-input.js
@@ -5,23 +5,41 @@ class LocationInput extends React.Component {
 	render() {
 		return (
 			<div className="location-input">
-				<Location />
-				<label for="location-input-dropdown">Location
-				</label>
-				<select name="location-input-dropdown">
-						<option value="sample1" selected>Sample 1</option>
-						<option value="sample1">Sample 2</option>
-				</select>
+				<div className="location-input-label">
+					<Location />
+					<label for="location-input-dropdown">Location</label>
+				</div>
+				
+				<div className="location-input-dropdown-div">
+					<select name="location-input-dropdown">
+							<option value="sample1" selected>Sample 1</option>
+							<option value="sample1">Sample 2</option>
+					</select>
+				</div>
+				
 
 				<style jsx> {`
-					label {
-						margin-left: 4px;
+					.location-input-label {
+						margin-bottom: 13px;
 						font-family: Roboto;
 						font-style: normal;
 						font-weight: normal;
 						font-size: 14px;
 						line-height: 16px;
 						color: #5F5F5F;
+					}
+
+					.location-input-label label {
+						margin-left: 4px;
+					}
+
+					.location-input-dropdown-div select {
+						margin-left: 9px;
+						min-width: 122px;
+						border: none;
+						border-bottom: 1px solid #5F5F5F;
+						background-color: #ffffff;
+						border-radius: 1px;
 					}
 				`}
 
