fa4832bdccfb61b421180ed87aa228f17f74bca3
= <siheonl@andrew.cmu.edu>
2019-07-21 15:05:39 +0900

    removed container styling in index.js, excluding the text at the component

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,6 @@ function Home() {
       <div className="container">
         <div>Welcome to Next.js!</div>
         <Event />
-        <style jsx> {`
-          .container {
-            width: 407px;
-            height: 497px;
-
-            background: #FFFFFF;
-            border: 1px solid #5F5F5F;
-            box-sizing: border-box;
-            box-shadow: 0px 4px 4px rgba (0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
-            backdrop-filter: blur(4px);
-            border-radius: 4px;
-          }
-        `}
-
-        </style>
       </div>
     
     )
