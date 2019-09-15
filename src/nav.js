572fab86e18ed25cfc1b7d5e01ba9298dd542617
= <siheonl@andrew.cmu.edu>
2019-09-14 22:22:45 -0700

    redesigning nav with new css

--- src/nav.js
+++ src/nav.js
@@ -1,32 +1,43 @@
 import React from 'react';
+import './stylesheets/nav.css'
+import { Link } from 'react-router-dom';
+
 export default class Nav extends React.Component {
-    render() {
-        return (
-            <nav style={{"position": "absolute", width: "1440px", height: "57px", left: "0px", top: "0px"}}>
-            <div style= {{"position":"absolute", "left":"2.22%", "right":"86.67%", "bottom":"4.75%", "top":"44%","background":"url(bleudot_new.svg)", "borderRadius":"10px"}}></div>
-            <div style = {{"position": "absolute",
-width: "80px",
-height: "22px",
-left: "113px",
-top: "20px",
+	render() {
+		return (
+			<nav className="main-nav">
+				<div className="nav-container">
+					<div className="nav-left-side">
+						<Link to="/" className="nav-logo">
+							<img alt="Bleu Dot" src={require('../public/bluedot_new.svg')} className="nav-logo-icon" href="./index.js"></img>
+						</Link>
 
-fontFamily: "Nunito",
-fontStyle: "normal",
-fontWeight: "normal",
-fontSize: "18px",
-lineHeight: "25px",
+						<div className="nav-page-btns">
+							<Link to="/"> Discover</Link>
+							<Link to="/"> Create</Link>
+							<Link to="/"> My Calendar</Link>
+						</div>
+					</div>
 
-color: "#E2E1E1",
-}}>Discover</div>
-            <div style={{"position":"absolute","width":"79px","height":"21px","left":"218px","top":"20px","fontFamily":"Comic Sans","fontStyle":"normal","fontWeight":"normal","fontSize":"18px","lineHeight":"25px","color":"#E2E1E1"}}>Create</div>
-            <div style={{"position":"absolute","width":"125px","height":"21px","left":"324px","top":"20px","fontFamily":"Nunito","fontStyle":"normal","fontWeight":"normal","fontSize":"18px","lineHeight":"25px","color":"#E2E1E1"}}>My Calendar</div>
-            <div style={{"position":"absolute","width":"36px","height":"35px","left":"90%","top":"13px","background":"url(user_white.svg)"}}></div>
-            <div style={{"position":"absolute","width":"36px","height":"35px","left":"95%","top":"13px","background":"url(settings_White.svg)"}}></div>
+					<div className="nav-right-side">
+						<Link to="/">
+							<img alt="Search" src={require('../public/magnifier_search_icon.svg')} className="nav-icon"></img>
+						</Link>
 
-            </nav>
+						<Link to="/">
+							<img alt="Setting" src={require('../public/magnifier_search_icon.svg')} className="nav-icon"></img>
+						</Link>
+
+						<Link to="/">
+							<img alt="Profile" src={require('../public/user_white.svg')} className="nav-icon"></img>
+						</Link>
+					</div>
+
+				</div>
+			</nav>
 
 
 
-        )
-    }
+		)
+	}
 }
