3bca4cb5207d3b83b817f09adfe2b5ada3af5a05
= <siheonl@andrew.cmu.edu>
2019-11-02 23:13:37 -0400

    migrating codes from backend

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './stylesheets/serimApp.css';
+import React from "react";
+import DataProvider from "./DataProvider";
+import Discover from './subcomponent/Discover';
+import Nav from './Nav';
+import './stylesheets/App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-      <div class="topnav-container">
-        <div class="topnav-menu">
-          <a class="active" href="discover">Discover</a>
-          <a href="create">Create </a>
-        </div>
-        <div class="topnav-title">
-          <a href="">BleuDot</a>
-        </div>
-      </div>
-
-      <div class="search-box">
-        Show me <input type="text" placeholder="multicultural, music, cooking..." name="search"/>
-        In <input type="text" placeholder="Community" name="search"/>
-      </div>
-
-    </div>
-
-
-  );
-}
+const App = () => (
+  <React.Fragment>
+    <Nav />
+    <DataProvider endpoint="http://bleudot-backend-api.herokuapp.com/events" 
+                  render={data => <Discover data={data} />} />
+  </React.Fragment>
+);
 
 export default App;
--- src/nav.js
+++ src/nav.js
@@ -1,43 +1,28 @@
-import React from 'react';
-import './stylesheets/nav.css'
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import Setting from "assets/setting.svg";
+import Profile from "assets/profile.svg";
 
-export default class Nav extends React.Component {
+
+class Nav extends Component {
 	render() {
 		return (
-			<nav className="main-nav">
-				<div className="nav-container">
-					<div className="nav-left-side">
-						<Link to="/" className="nav-logo">
-							<img alt="Bleu Dot" src={require('./public/bleudot_new.svg')} className="nav-logo-icon" href="./index.js"></img>
-						</Link>
-
-						<div className="nav-page-btns">
-							<Link to="/" className="nav-page-btn"> Discover</Link>
-							<Link to="/" className="nav-page-btn"> Create</Link>
-							<Link to="/" className="nav-page-btn"> My Calendar</Link>
-						</div>
+			<div className="navbar">
+				<div className="navbar-container container">
+					<div className="navbar-menu">
+						<a className="active" href=".">Discover</a>
+						<a href=".">Create </a>
 					</div>
-
-					<div className="nav-right-side">
-						<Link to="/">
-							<img alt="Search" src={require('./public/magnifier_search_icon.svg')} className="nav-icon"></img>
-						</Link>
-
-						<Link to="/">
-							<img alt="Setting" src={require('./public/settings_White.svg')} className="nav-icon"></img>
-						</Link>
-
-						<Link to="/">
-							<img alt="Profile" src={require('./public/user_white.svg')} className="nav-icon"></img>
-						</Link>
+					<div className="navbar-title">
+						<a href=".">BleuDot </a>
 					</div>
-
+					<div className="navbar-buttons">
+						<div className="navbar-button-icon"><img src={Setting} /></div>
+						<div className="navbar-button-icon"><img src={Profile} /></div>
+					</div>
 				</div>
-			</nav>
-
-
-
-		)
+			</div>
+		);
 	}
 }
+
+export default Nav;
