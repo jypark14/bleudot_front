03a533817e73f10db475a079ddebd3121c30d74c
= <siheonl@andrew.cmu.edu>
2019-11-03 23:12:31 -0500

    api sends data and it shows directly

--- src/subcomponent/Discover.js
+++ src/subcomponent/Discover.js
@@ -12,30 +12,30 @@ class Discover extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      upcoming: ["SWE", "ECE", "IEEE", "Build", "5", "6"],
-      trending: ["1", "2", "3", "4", "5", "6"],
-      near: ["1", "2", "3", "4"],
-      event_upcoming: {
-        title: ["Korean Food Sale", "ASA Boba Sale", "3 Sale", "4 Sale", "5 Sale", "6 Sale"],
-        org: ["Korean Student Association", "Asian Student Organization", "3 Org", "4 Org", "5 Org", "6 Org"],
-        day: ["28th", "29th", "30th", "31th", "32th", "33th"],
-        month: ["OCT", "OCT", "OCT", "OCT", "OCT", "OCT"],
-        time: ["10 AM - 2 PM", "1PM - 3PM", "3PM - 5PM", "6PM- 8PM", "8PM - 10PM", "11PM - 1AM"],
-      },
-      event_trending: {
-        title: ["Korean Food Sale", "ASA Boba Sale", "3 Sale", "4 Sale", "5 Sale", "6 Sale"],
-        org: ["Korean Student Association", "Asian Student Organization", "3 Org", "4 Org", "5 Org", "6 Org"],
-        day: ["28th", "29th", "30th", "31th", "32th", "33th"],
-        month: ["OCT", "OCT", "OCT", "OCT", "OCT", "OCT"],
-        time: ["10 AM - 2 PM", "1PM - 3PM", "3PM - 5PM", "6PM- 8PM", "8PM - 10PM", "11PM - 1AM"],
-      },
-      event_: {
-        title: ["Korean Food Sale", "ASA Boba Sale", "3 Sale", "4 Sale", "5 Sale", "6 Sale"],
-        org: ["Korean Student Association", "Asian Student Organization", "3 Org", "4 Org", "5 Org", "6 Org"],
-        day: ["28th", "29th", "30th", "31th", "32th", "33th"],
-        month: ["OCT", "OCT", "OCT", "OCT", "OCT", "OCT"],
-        time: ["10 AM - 2 PM", "1PM - 3PM", "3PM - 5PM", "6PM- 8PM", "8PM - 10PM", "11PM - 1AM"],
-      },
+      // upcoming: ["SWE", "ECE", "IEEE", "Build", "5", "6"],
+      // trending: ["1", "2", "3", "4", "5", "6"],
+      // near: ["1", "2", "3", "4"],
+      // event_upcoming: {
+      //   title: ["Korean Food Sale", "ASA Boba Sale", "3 Sale", "4 Sale", "5 Sale", "6 Sale"],
+      //   org: ["Korean Student Association", "Asian Student Organization", "3 Org", "4 Org", "5 Org", "6 Org"],
+      //   day: ["28th", "29th", "30th", "31th", "32th", "33th"],
+      //   month: ["OCT", "OCT", "OCT", "OCT", "OCT", "OCT"],
+      //   time: ["10 AM - 2 PM", "1PM - 3PM", "3PM - 5PM", "6PM- 8PM", "8PM - 10PM", "11PM - 1AM"],
+      // },
+      // event_trending: {
+      //   title: ["Korean Food Sale", "ASA Boba Sale", "3 Sale", "4 Sale", "5 Sale", "6 Sale"],
+      //   org: ["Korean Student Association", "Asian Student Organization", "3 Org", "4 Org", "5 Org", "6 Org"],
+      //   day: ["28th", "29th", "30th", "31th", "32th", "33th"],
+      //   month: ["OCT", "OCT", "OCT", "OCT", "OCT", "OCT"],
+      //   time: ["10 AM - 2 PM", "1PM - 3PM", "3PM - 5PM", "6PM- 8PM", "8PM - 10PM", "11PM - 1AM"],
+      // },
+      // event_: {
+      //   title: ["Korean Food Sale", "ASA Boba Sale", "3 Sale", "4 Sale", "5 Sale", "6 Sale"],
+      //   org: ["Korean Student Association", "Asian Student Organization", "3 Org", "4 Org", "5 Org", "6 Org"],
+      //   day: ["28th", "29th", "30th", "31th", "32th", "33th"],
+      //   month: ["OCT", "OCT", "OCT", "OCT", "OCT", "OCT"],
+      //   time: ["10 AM - 2 PM", "1PM - 3PM", "3PM - 5PM", "6PM- 8PM", "8PM - 10PM", "11PM - 1AM"],
+      // },
     };
   }
 
@@ -45,29 +45,25 @@ class Discover extends Component {
     return (
       <div className="container">
         <Logo/>
-        <div className="filter-btns">
-          <button className="filter-btn">Organizations</button>
-          <button className="filter-btn">Events</button>
-        </div>
         {categories.map(((category, id) => {
           return (
             <div className="category">
               <div className="title">{category}</div>
               {(id === 0) ?
                 (
-                  <Events type="upcoming" categoryName={this.state.upcoming} event={this.state.event_upcoming}></Events>
+                  <Events type="upcoming" categoryName={this.props.data} topic="upcoming"></Events>
                 ) :
                 (<div></div>)
               }
               {(id === 1) ?
                 (
-                <Events categoryName={this.state.trending} event={this.state.event_trending}></Events>
+                <Events categoryName={this.props.data} topic="trending"></Events>
                 ) :
                 (<div></div>)
               }
               {(id === 2) ?
                 (
-                  <Events categoryName={this.state.near} event={this.state.event_}></Events>
+                  <Events categoryName={this.props.data} topic="near"></Events>
                 ) :
                 (<div></div>)
               }
