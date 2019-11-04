f9055d1534bee289457d8780bd258cf5bd0a4838
= <siheonl@andrew.cmu.edu>
2019-11-03 23:12:13 -0500

    changed code structure but the way it works is totally same

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -2,28 +2,42 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class DataProvider extends Component {
+
+  state = {
+    data: [],
+    loaded: false,
+    placeholder: "Loading..."
+  };
+
   static propTypes = {
     endpoint: PropTypes.string.isRequired,
     render: PropTypes.func.isRequired
   };
-  state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading..."
-    };
-  componentDidMount() {
-    fetch(this.props.endpoint)
-      .then(response => {
-        if (response.status !== 200) {
-          return this.setState({ placeholder: "Something went wrong" });
-        }
-        return response.json();
-      })
-      .then(data => this.setState({ data: data, loaded: true }));
+
+  _getData = async () => {
+    const data = await this._callApi();
+    console.log(data);
+    this.setState({
+      data: data,
+      loaded: true
+    })
   }
+
+  _callApi = () => {
+    return fetch(this.props.endpoint)
+    .then(response => {
+      const json = response.json()
+      return json
+    })
+    .catch(err => this.setState({ placeholder: `Err msg: ${err}`}))
+  }
+
+  componentDidMount() {
+    this._getData();
+  }
+
   render() {
-    const { data, loaded, placeholder } = this.state;
-    return loaded ? this.props.render(data) : <p>{placeholder}</p>;
+    return this.state.loaded ? this.props.render(this.state.data) : <p>{this.state.placeholder}</p>;
   }
 }
 export default DataProvider;
